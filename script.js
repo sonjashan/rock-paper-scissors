a917730d92b9e6c4fba77db2f65b3ccebcc646cf
sonjashan <102500611+sonjashan@users.noreply.github.com>
2024-10-18 21:03:06 -0400

    add fn: randomly generates rock paper or scissors

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+'use strict';
+
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 10);
+    if (num > 6) return `Rock`;
+    else if (num > 3) return `Paper`;
+    else return `Scissors`;
+}
+console.log(getComputerChoice());
